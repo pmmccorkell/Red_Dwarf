f16278c64b69a09d98e136b8498d60a00343e1db
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-03 14:51:01 -0400

    Cron Commit

--- multiprocessing/plot_test.py
+++ multiprocessing/plot_test.py
@@ -290,13 +290,15 @@ class Plotting:
 		self.ax1.clear()
 		line1,=self.ax1.plot(self.x, self.y1,lw=2,color='b',label='bno')
 		line2,=self.ax1.plot(self.x, self.y2,lw=2,color='r',label='qtm')
-		self.ax1.legend()
-		if (not self.BLIT):
+
+		if (self.BLIT):
+			self.ax1.legend()
+			return line1,line2
+
+		else:
 			self.ax1.set_ylim(-1000,1500)
 			self.ax1.legend()
 
-		if self.BLIT:
-			return line1,line2
 
 
 def set_core_affinity(x=0):
