e278387a947030728ac72a0985fb67be9b1e0aff
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-03 13:38:01 -0400

    Cron Commit

--- multiprocessing/plot_test.py
+++ multiprocessing/plot_test.py
@@ -269,6 +269,11 @@ class Plotting:
 
 		plt.show()
 
+	def init(self):
+		line1.set_data([],[])
+		line2.set_data([],[])
+		return line1,line2
+
 	def animate_qtm(self,i,line1,line2):
 		# self.read_in_data()
 		self.x.pop(0)
@@ -281,8 +286,8 @@ class Plotting:
 		self.y2.append(self.qtm['heading'])
 
 		# self.ax1.clear()
-		line1=self.ax1.plot(self.x, self.y1,lw=2,color='b',label='bno')
-		line2=self.ax1.plot(self.x, self.y2,lw=2,color='r',label='qtm')
+		# line1=self.ax1.plot(self.x, self.y1,lw=2,color='b',label='bno')
+		# line2=self.ax1.plot(self.x, self.y2,lw=2,color='r',label='qtm')
 		self.ax1.legend()
 		# artists = [line1,line2]
 		# Artists = namedtuple("Artists",("bno","qtm"))
