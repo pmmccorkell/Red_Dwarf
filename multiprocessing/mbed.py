8bcdd39703e4d495add45031634efc40574a32ca
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-07 14:43:40 -0400

    Cron Commit

--- multiprocessing/mbed.py
+++ multiprocessing/mbed.py
@@ -179,6 +179,9 @@ def get_angles():
 
 	#Read bytes from Serial
 	in_buffer=ser.readline()
+
+	if DEBUG:
+		print(in_buffer)
 	
 	#Only assign prefix and data if serial line is correct length
 	#Prevents Value Errors
