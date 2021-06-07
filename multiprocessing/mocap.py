8d82b6e180a3bf1db949b5d5e0b95e5820515fc9
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-07 15:28:22 -0400

    Cron Commit

--- multiprocessing/mocap.py
+++ multiprocessing/mocap.py
@@ -118,7 +118,21 @@ def stream_data():
 		if buffer:
 			data_in = buffer
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+	
 
 if __name__ == '__main__':
 	print('running as main')
+	import socket
+
 	stream_data()
