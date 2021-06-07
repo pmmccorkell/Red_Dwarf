3fdd5a8f485de024bf4ae8bdb7a6142c44024d09
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-07 13:50:39 -0400

    Cron Commit

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -113,7 +113,7 @@ def pwm_controller_thread():
 ##################################################################################
 
 def xbox_process_setup():
-	global xb_pipe_in,xbox_process,xbox_controller
+	global xb_pipe_in,xb_pipe_out,xbox_process,xbox_controller
 	xb_pipe_in, xb_pipe_out = Pipe()
 	xbox_controller = xb.XBoxController(xb_pipe_in)
 	xbox_controller.max_speed = max_speed
