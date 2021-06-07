fc528abb64061f4ce0f6e9391c0a99817491b987
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-07 14:20:27 -0400

    Cron Commit

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -137,9 +137,7 @@ def xbox_read():
 	global vessel, qtm, bno, measured_active
 	# read_pipe = xb_pipe_in
 	buffer = {}
-	print('start xbox_read')
-	# print(xb_pipe_out.recv())
-	# print(xb_pipe_in.recv())
+	# print('start xbox_read')
 	while (xb_pipe_out.poll()):
 		# print('xb_pipe poll')
 		buffer = xb_pipe_out.recv()
