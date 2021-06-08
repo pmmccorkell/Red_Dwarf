0ca7389b1de5578534e13132143ca760d0ae5fd6
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-08 13:22:16 -0400

    Cron Commit

--- multiprocessing/plotting.py
+++ multiprocessing/plotting.py
@@ -159,6 +159,8 @@ if __name__ == '__main__':
 	data_thread = Thread(target=update_data,daemon=True)
 	data_thread.start()
 
+	plot_process_setup()
+
 	# array_thread = Thread(target=form_data,daemon=True)
 	# array_thread.start()
 
