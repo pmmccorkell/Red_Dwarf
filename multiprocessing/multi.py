3c58102605702ed3206334d280b289a6b7fbba50
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-08 13:49:00 -0400

    Cron Commit

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -348,7 +348,8 @@ def setup():
 	mbed_thread = Thread(target=mbed_stream,daemon=daemon_mode)
 	mbed_thread.start()
 
-	plot_thread = Thread(target=plotting,daemon=daemon_mode)
+	plot_process_setup
+	plot_thread = Thread(target=plot_stream,daemon=daemon_mode)
 	plot_thread.start()
 
 def loop():
