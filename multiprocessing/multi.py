7a03ee3e442ca94b53584469469d3a9f3cdb9acd
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-09 11:42:11 -0400

    Cron Commit

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -136,8 +136,9 @@ def xbox_read():
 		# 	vessel.issueCommand('vel',999)
 		vessel.persistent_speed = bool(max(buffer['speed']-10,0)) * buffer['speed']
 
-		buffer['maintain'] = 
+		buffer['maintain'] = xbox_debounce(xbox['maintain'],buffer['maintain'])
 		# buffer['maintain'] = xbox['maintain'] ^ buffer['maintain']
+		buffer['mode'] = xbox_debounce(xbox['mode'],buffer['mode'])
 		# buffer['mode'] = xbox['mode'] ^ buffer['mode']
 
 		for k in measured_active:
