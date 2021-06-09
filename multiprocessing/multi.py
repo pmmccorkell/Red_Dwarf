26cc03c5ef589ee52f28a0c1ca0f0d9e02c519e6
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-09 12:48:51 -0400

    Cron Commit

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -369,7 +369,7 @@ def setup():
 
 	plot_process_setup
 	plot_thread = Thread(target=plot_stream,daemon=daemon_mode)
-	plot_thread.start()
+	# plot_thread.start()
 
 def loop():
 	while(not xbox['quit']):
