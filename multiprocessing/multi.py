84e2870529de7d99e0afa8528208d699d0c9eb7b
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-09 13:57:58 -0400

    Cron Commit

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -119,6 +119,7 @@ def xbox_debounce(val1,val2):
 	debounce_timer = (not not_timer_bool * monotonic()) + (not_timer_bool * debounce_timer)
 	# print('current: '+str(current)+', db timer: '+str(debounce_timer)+', bool: '+str(bool_val))
 	# print('returnval: '+str(returnval))
+	print(debounce_timer)
 	# debounce_timer = (monotonic()*val2) + (debounce_timer * (not val2))
 	return returnval
 
