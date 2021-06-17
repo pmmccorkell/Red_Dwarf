341128a548e6accec20ca624e51b680b072a24de
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-17 13:54:53 -0400

    Cron Commit

--- testing ideas/oled_test.py
+++ testing ideas/oled_test.py
@@ -166,6 +166,10 @@ class Joystick:
 oled = OLED()
 dpad = Joystick(oled)
 
+def check_diag():
+
+dpad.direction_up = check_diag()
+
 def update():
 	events = dpad.dpad.get_events()
 	for event in events:
