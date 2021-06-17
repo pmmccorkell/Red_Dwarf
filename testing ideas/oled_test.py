829c8ba2aeb76812186df839b1b7802768e4378e
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-17 10:49:23 -0400

    Cron Commit

--- testing ideas/oled_test.py
+++ testing ideas/oled_test.py
@@ -80,9 +80,9 @@ class OLED()
 def update(self):
 	events = self.dpad.get_events()
 	for event in events:
-		if event.action=="pressed"
-		joystick_line = event.direction+" "+event.action
-		self.update_stats(joystick_line)
+		if event.action=="pressed":
+			joystick_line = event.direction+" "+event.action
+			self.update_stats(joystick_line)
 
 
 # First define some constants to allow easy resizing of shapes.
