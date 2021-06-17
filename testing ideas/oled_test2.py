3a7f82ae23645618217e704c3789dc5324db202d
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-17 14:58:40 -0400

    Cron Commit

--- testing ideas/oled_test2.py
+++ testing ideas/oled_test2.py
@@ -149,10 +149,10 @@ def update():
 
 def update_state():
 	# oled.update_stats(dpad.last_event)
-	if dpad.UPLEFT:
-		print("up left")
-	if dpad.UPRIGHT:
-		print("up right")
+	# if dpad.UPLEFT:
+	# 	print("up left")
+	# if dpad.UPRIGHT:
+	# 	print("up right")
 	if (dpad.MIDDLE):
 		return 0
 	return 1
