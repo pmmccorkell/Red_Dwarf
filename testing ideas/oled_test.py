56160f27b829944d767ad490d95abac0d1401870
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-17 13:52:59 -0400

    Cron Commit

--- testing ideas/oled_test.py
+++ testing ideas/oled_test.py
@@ -110,7 +110,7 @@ class Joystick:
 		check_val = self.get_ord(event.action)
 		check_released = bool(check_val - self.released_val)
 		check_hold = bool(check_val - self.held_val)
-		self.MIDDLE = 1 * check_released
+		self.LEFT = 1 * check_released
 		self.last_event = event.direction
 		self.event_queue.append(check_released * check_hold * event.direction)
 		# if check_val:
