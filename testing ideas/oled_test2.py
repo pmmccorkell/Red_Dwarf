9325bb27e78a22a635ff33abe4e8b30f75e03b36
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-22 08:22:46 -0400

    Cron Commit

--- testing ideas/oled_test2.py
+++ testing ideas/oled_test2.py
@@ -101,7 +101,7 @@ class Joystick:
 
 		# self.change_inputs()
 
-	# All this just to run "xinput float x", where x is stringified integer relating the IO device id for the Joystick.
+	# All this just to run "xinput float x" in bash, where x is stringified integer relating the IO device id for the Joystick.
 	def silence_xinput(self):
 		buffer = subprocess.Popen(["xinput"],stdout=subprocess.PIPE)
 		buffer2 = subprocess.run(["grep","Raspberry"],stdin=buffer.stdout,stdout=subprocess.PIPE)
