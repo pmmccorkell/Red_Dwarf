8100bf469d07dbdb0cfa48116a8ff719eceea4e7
root <root@WRK1072778GOVT.academy.usna.edu>
2021-06-22 08:33:52 -0400

    Cron Commit

--- testing ideas/oled_test2.py
+++ testing ideas/oled_test2.py
@@ -109,10 +109,9 @@ class Joystick:
 		buffer3 = buffer2.stdout.decode()
 		io_id = buffer3.find('id=')
 
+		# In case the ID is 2 digits or more, iterate over a few digits until the 'tab' is found.
 		buffer4=''
 		i=3
-
-		# In case the ID is 2 digits or more, iterate over a few digits until the 'tab' is found.
 		while ((not buffer4.endswith('\t')) and i<8):
 			buffer4 += buffer3[io_id + i]
 			i+=1
