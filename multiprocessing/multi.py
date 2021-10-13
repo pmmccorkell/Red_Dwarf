a56fca990f1c8b52d0628e56308fd734fab3fc89
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-10-13 09:34:03 -0400

    wsl auto

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -398,7 +398,7 @@ def setup():
 	xbox_thread = Thread(target=xbox_stream,daemon=daemon_mode)
 	xbox_thread.start()
 
-	plot_process_setup
+	plot_process_setup()
 	plot_thread = Thread(target=plot_stream,daemon=daemon_mode)
 	plot_thread.start()
 
