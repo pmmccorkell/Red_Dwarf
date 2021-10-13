a39a6bda395c4fb56d7650f7b78bddb602c72f13
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-10-13 10:48:23 -0400

    wsl auto

--- multiprocessing/plotting.py
+++ multiprocessing/plotting.py
@@ -82,8 +82,8 @@ class Plotting:
 				self.qtm = buffer['qtm']
 				self.bno = buffer['bno']
 				self.form_data()
-				delimiter = ','
-				
+				delimiter = ', '
+
 		sleep(0.001)
 	
 	def form_data(self):
