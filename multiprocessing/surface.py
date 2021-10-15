131d6aa5d82f39533721fd0e9ea1ae8d36cb4bea
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-10-15 08:01:06 -0400

    wsl auto

--- multiprocessing/surface.py
+++ multiprocessing/surface.py
@@ -163,7 +163,7 @@ class Controller():
 
 	def incrementHeading(self,magnitude):
 		heading_resolution=3	# degrees
-		self.persistent_heading = self.persistent_heading+(magnitude*heading_resolution)
+		self.persistent_heading = self.clampyclamp(self.rangeHea,self.persistent_heading+(magnitude*heading_resolution))
 		self.pidHeading.clear()
 	def incrementSpeed(self,magnitude):
 		speed_resolution=27		# us
