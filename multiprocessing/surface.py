430cb2f814fe8f697b103690b072dfa4cb1a9d12
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-10-15 08:22:51 -0400

    wsl auto

--- multiprocessing/surface.py
+++ multiprocessing/surface.py
@@ -130,10 +130,6 @@ class Controller():
 
 
 	def azThrusterLogic(self):
-		# Error check values fed to controllers are in expected format / range.
-		# 	ie 0 to 360 vs -180 to 180
-		self.wraparound(self.rangeHea,self.heading)
-		self.wraparound(self.rangeHea,self.persistent_heading)
 		self.wraparound(self.rangeOff,self.persistent_offset)
 		self.clampyclamp(self.rangeVel,self.persistent_speed)
 
