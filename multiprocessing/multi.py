b3669198f2bc824b1ae4cdf2deafcaa325651c35
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-10-15 09:37:50 -0400

    wsl auto

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -178,7 +178,7 @@ def xbox_read():
 		xbox = buffer
 
 def xbox_stream():
-	global xbox, measured_active, xbox_interval, xbox_flag
+	global measured_active, xbox_interval, xbox_flag
 	interval = xbox_interval
 	while(xbox_flag.set_flag()):
 		start = monotonic()+interval
