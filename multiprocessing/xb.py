ea0269604e3742c889c8965d1091498cff9f6709
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-10-15 07:34:23 -0400

    wsl auto

--- multiprocessing/xb.py
+++ multiprocessing/xb.py
@@ -122,6 +122,11 @@ class XBoxController:
 		print("turning off xbox controller")
 		self.joystick.close()
 
+
+
+###############################################
+######### FOR DEBUGGING PURPOSES ONLY #########
+###############################################
 def xbox_debounce(val1,val2):
 	global debounce_timer, debounce_time_check
 	current = monotonic()
@@ -139,9 +144,6 @@ def xbox_debounce(val1,val2):
 	return returnval
 
 
-###############################################
-######### FOR DEBUGGING PURPOSES ONLY #########
-###############################################
 if __name__ == '__main__':
 	print("running as main")
 	print("Importing libraries for independent mode.")
