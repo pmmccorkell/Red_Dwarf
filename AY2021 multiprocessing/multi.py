3085b4c88f72d909319ba6802ece9ede168b6005
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-11-19 12:23:50 -0500

    wsl auto

--- AY2021 multiprocessing/multi.py
+++ AY2021 multiprocessing/multi.py
@@ -28,8 +28,8 @@ max_speed = 500   # us		speed limit
 rigid_body_name = 'RedDwarf'
 # qtm_server = '192.168.5.4'	# SURF qtm server
 # qtm_server = '192.168.42.24'	# Pat's TSD office desktop on Robotics wireless
-# qtm_server = '10.60.17.246'		# Pat's TSD office desktop on Mission wired
-qtm_server = '10.25.56.113'		# Pat's TSD office desktop on Mission wireless
+qtm_server = '10.60.17.246'		# Pat's TSD office desktop on Mission wired
+# qtm_server = '10.25.56.113'		# Pat's TSD office desktop on Mission wireless
 
 pwm_interval = 0.02		# seconds
 qtm_interval = 0.005	# seconds
