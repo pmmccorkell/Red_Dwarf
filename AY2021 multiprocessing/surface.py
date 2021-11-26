4957f6c7bf632e81333bd5617c16a1a16339518f
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2021-11-26 11:20:43 -0500

    wsl auto

--- AY2021 multiprocessing/surface.py
+++ AY2021 multiprocessing/surface.py
@@ -141,25 +141,25 @@ class Controller():
 		self.thrusters.stopAllThrusters()
 
 	def wraparound(self,rangen,n):
-		print('enter: '+str(n))
-		minn = min(rangen)
-		maxn = max(rangen)
-		if n < minn:
-			n += 2*maxn
-		if n > maxn:
-			n += 2*minn
-		print('return: '+str(n))
-		return n
-		# if ((n is None) or (n in rangen)):
-		# 	print('n: ' +str(n))
-		# 	return n
-		# else:
-		# 	minn = min(rangen)
-		# 	maxn = max(rangen)
-		# 	if (n < minn):
-		# 		self.wraparound(rangen,(n + (2 * maxn)))
-		# 	elif (n > maxn):
-		# 		self.wraparound(rangen, (n + (2 * minn)))
+		# print('enter: '+str(n))
+		# minn = min(rangen)
+		# maxn = max(rangen)
+		# if n < minn:
+		# 	n += 2*maxn
+		# if n > maxn:
+		# 	n += 2*minn
+		# print('return: '+str(n))
+		# return n
+		if ((n is False) or (n in rangen)):
+			print('n: ' +str(n))
+			return n
+		else:
+			minn = min(rangen)
+			maxn = max(rangen)
+			if (n < minn):
+				self.wraparound(rangen,(n + (2 * maxn)))
+			elif (n > maxn):
+				self.wraparound(rangen, (n + (2 * minn)))
 		
 	def clampyclamp(self,rangen,n):
 		minn = min(rangen)
