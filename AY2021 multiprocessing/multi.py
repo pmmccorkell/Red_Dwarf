0d0e73960d1fa08da207733d1221f39a0d54f885
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2022-03-23 12:42:08 -0400

    wsl auto

--- AY2021 multiprocessing/multi.py
+++ AY2021 multiprocessing/multi.py
@@ -170,7 +170,7 @@ xbox = {
 	'speed':0,
 	'graph':0,		# graph 1 toggles plotter to start/stop
 	'maintain':1,	# maintain 1 attempts to stay at last heading command
-	'mode':1,		# mode 1 qtm, mode 0 bno
+	'mode':1,		
 	'quit':0
 }
 def xbox_read():
