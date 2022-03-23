07f3e38b4fac1cf718f41603735135cbc5a772d4
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2022-03-23 12:13:36 -0400

    wsl auto

--- AY2021 multiprocessing/multi.py
+++ AY2021 multiprocessing/multi.py
@@ -170,6 +170,7 @@ def xbox_read():
 		buffer['maintain'] = xbox_debounce(xbox['maintain'],buffer['maintain'])
 		buffer['mode'] = xbox_debounce(xbox['mode'],buffer['mode'])
 		measured_active_index = (measured_active_index + (last_mode ^ buffer['mode'])) % 3
+		last_mode = buffer['mode']
 
 		# for k in measured_active:
 		# 	measured_active[k] = (buffer['mode'] * qtm[k]) + ((not buffer['mode']) * bno[k])
