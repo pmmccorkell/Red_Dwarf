5adab807d15688a675e9ad9cd524b8fef97f6cc0
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2022-03-23 12:31:50 -0400

    wsl auto

---
+++ AY2021 multiprocessing/secrets.py
@@ -0,0 +1,4 @@
+# qtm_server = '192.168.5.4'	# SURF qtm server
+# qtm_server = '192.168.42.24'	# Pat's TSD office desktop on Robotics wireless
+qtm_server = '10.60.17.245'		# Pat's TSD office desktop on Mission wired
+# qtm_server = '10.25.56.113'		# Pat's TSD office desktop on Mission wireless
