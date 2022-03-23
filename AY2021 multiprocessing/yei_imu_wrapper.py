5ec72137dc757383d321de3d84aaccc8139c958a
wsl desktop <78446270+pmmccorkell-usna@users.noreply.github.com>
2022-03-23 11:56:04 -0400

    wsl auto

--- AY2021 multiprocessing/yei_imu_wrapper.py
+++ AY2021 multiprocessing/yei_imu_wrapper.py
@@ -60,5 +60,11 @@ if __name__ == '__main__':
 	yei_process = Process(target=yei_imu.stream,daemon=daemon_mode)
 	yei_process.start()
 
+yei = {
+	'timestamp':999,
+	'heading':999,
+	'roll':999,
+	'pitch':999
+}
 
 
