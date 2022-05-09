cffd3dbbac4a7f3d5eefc982b9d6f00b8dbb97df
pmmccorkell-usna <78446270+pmmccorkell-usna@users.noreply.github.com>
2022-05-09 13:20:22 -0400

    asdf

--- tests/Timing/qtm server to raspi tcpdump/confInterval.m
+++ tests/Timing/qtm server to raspi tcpdump/confInterval.m
@@ -11,7 +11,6 @@ classdef confInterval < matlab.mixin.SetGet
         mn
         std_dev
         std_err
-        tscore
         conf_interval
         conf_interval_range
     end % properties end
@@ -35,8 +34,8 @@ classdef confInterval < matlab.mixin.SetGet
             obj.mn = mean(obj.data_set);
             obj.std_dev = std(obj.data_set);
             obj.std_err = obj.std_dev / (sqrt(length(obj.data_set)));
-            obj.tscore = tinv([1-obj.alpha obj.alpha],length(obj.data_set)-1);
-            obj.conf_interval = obj.mn + (obj.tscore * obj.std_err);
+            tscore = tinv([1-obj.alpha obj.alpha],length(obj.data_set)-1);
+            obj.conf_interval = obj.mn + (tscore * obj.std_err);
             obj.conf_interval_range = obj.conf_interval(2) - obj.conf_interval(1);
         end % calculate end
 
